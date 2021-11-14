0bfa891e6001221622999a7827ab70f8604f1529
orrin <44979097+orrinrhodes@users.noreply.github.com>
2021-11-13 20:18:59 -0500

    Wrong code lol

--- tos.py
+++ tos.py
@@ -1,19 +1,249 @@
-print('hey lol');
-
 import os.path
 from tkinter import *
+from pynput.mouse import Button as Key, Controller #tkinter button
+mouse = Controller()
+
+#get Isaac -- absolutely essential for QoL tings
+cd = os.getcwd();
+icon = cd+'\\tos\isaacicon.ico'
+
+widget = Tk()
+widget.iconbitmap(icon)
+
+ws = widget.winfo_screenwidth()
+hs = widget.winfo_screenheight()
+#print(ws,'x', hs)
 
 #get Isaac
-cd = os.getcwd();
+cd = os.getcwd()
 icon = cd+'\\tos\images\isaacicon.ico'
 
-#tos app
-tos = Tk();
-#QoL tings
-tos.title('ToS Simulator');
-tos.iconbitmap(icon);
+#pretty colors
+widget.title('Productivity')
+bg = 'green'
+fg = 'yellow'
+box = Label(
+    widget,
+    text='BE PRODUCTIVE',
+    font=('digital numbers',20),
+    background = bg, foreground=fg
+    )
+box.pack(anchor='center')
+box.master.wm_attributes('-alpha',0.9)
+#borderlesss
+box.master.overrideredirect(True)
 
-tos.geometry('500x500+500+1000')
+#function
+def exit():
+    widget.destroy()
 
-#GO!
-tos.mainloop();
+def msg():
+    print('hey lol')
+
+exit1 = Button(widget, text='Click to exit', command=exit)
+exit1.pack(pady=2)
+
+#above all
+box.master.wm_attributes('-topmost',True)
+box.master.lift()
+
+#position
+x = 200; y = 200
+def pos():
+    mpos = mouse.position
+    new = '{0}x{1}+{2}+{3}'.format(x,y,round(mpos[0]-x/2),round(mpos[1]-y/2))
+    widget.geometry(new)
+    widget.after(1,pos)
+pos()
+#GO
+widget.mainloop()
+
+
+if str(type(True))=="<class 'bool'>" and True!=False:
+    print(True,'is True!')
+if str(type(True))=="<class 'bool'>" and True!=False:
+    print(True,'is True!')
+if str(type(True))=="<class 'bool'>" and True!=False:
+    print(True,'is True!')
+if str(type(True))=="<class 'bool'>" and True!=False:
+    print(True,'is True!')
+if str(type(True))=="<class 'bool'>" and True!=False:
+    print(True,'is True!')
+if str(type(True))=="<class 'bool'>" and True!=False:
+    print(True,'is True!')
+if str(type(True))=="<class 'bool'>" and True!=False:
+    print(True,'is True!')
+if str(type(True))=="<class 'bool'>" and True!=False:
+    print(True,'is True!')
+if str(type(True))=="<class 'bool'>" and True!=False:
+    print(True,'is True!')
+if str(type(True))=="<class 'bool'>" and True!=False:
+    print(True,'is True!')
+if str(type(True))=="<class 'bool'>" and True!=False:
+    print(True,'is True!')
+if str(type(True))=="<class 'bool'>" and True!=False:
+    print(True,'is True!')
+if str(type(True))=="<class 'bool'>" and True!=False:
+    print(True,'is True!')
+if str(type(True))=="<class 'bool'>" and True!=False:
+    print(True,'is True!')
+if str(type(True))=="<class 'bool'>" and True!=False:
+    print(True,'is True!')
+if str(type(True))=="<class 'bool'>" and True!=False:
+    print(True,'is True!')
+if str(type(True))=="<class 'bool'>" and True!=False:
+    print(True,'is True!')
+if str(type(True))=="<class 'bool'>" and True!=False:
+    print(True,'is True!')
+if str(type(True))=="<class 'bool'>" and True!=False:
+    print(True,'is True!')
+if str(type(True))=="<class 'bool'>" and True!=False:
+    print(True,'is True!')
+if str(type(True))=="<class 'bool'>" and True!=False:
+    print(True,'is True!')
+if str(type(True))=="<class 'bool'>" and True!=False:
+    print(True,'is True!')
+if str(type(True))=="<class 'bool'>" and True!=False:
+    print(True,'is True!')
+if str(type(True))=="<class 'bool'>" and True!=False:
+    print(True,'is True!')
+if str(type(True))=="<class 'bool'>" and True!=False:
+    print(True,'is True!')
+if str(type(True))=="<class 'bool'>" and True!=False:
+    print(True,'is True!')
+if str(type(True))=="<class 'bool'>" and True!=False:
+    print(True,'is True!')
+if str(type(True))=="<class 'bool'>" and True!=False:
+    print(True,'is True!')
+if str(type(True))=="<class 'bool'>" and True!=False:
+    print(True,'is True!')
+if str(type(True))=="<class 'bool'>" and True!=False:
+    print(True,'is True!')
+if str(type(True))=="<class 'bool'>" and True!=False:
+    print(True,'is True!')
+if str(type(True))=="<class 'bool'>" and True!=False:
+    print(True,'is True!')
+if str(type(True))=="<class 'bool'>" and True!=False:
+    print(True,'is True!')
+if str(type(True))=="<class 'bool'>" and True!=False:
+    print(True,'is True!')
+if str(type(True))=="<class 'bool'>" and True!=False:
+    print(True,'is True!')
+if str(type(True))=="<class 'bool'>" and True!=False:
+    print(True,'is True!')
+if str(type(True))=="<class 'bool'>" and True!=False:
+    print(True,'is True!')
+if str(type(True))=="<class 'bool'>" and True!=False:
+    print(True,'is True!')
+if str(type(True))=="<class 'bool'>" and True!=False:
+    print(True,'is True!')
+if str(type(True))=="<class 'bool'>" and True!=False:
+    print(True,'is True!')
+if str(type(True))=="<class 'bool'>" and True!=False:
+    print(True,'is True!')
+if str(type(True))=="<class 'bool'>" and True!=False:
+    print(True,'is True!')
+if str(type(True))=="<class 'bool'>" and True!=False:
+    print(True,'is True!')
+if str(type(True))=="<class 'bool'>" and True!=False:
+    print(True,'is True!')
+if str(type(True))=="<class 'bool'>" and True!=False:
+    print(True,'is True!')
+if str(type(True))=="<class 'bool'>" and True!=False:
+    print(True,'is True!')
+if str(type(True))=="<class 'bool'>" and True!=False:
+    print(True,'is True!')
+if str(type(True))=="<class 'bool'>" and True!=False:
+    print(True,'is True!')
+if str(type(True))=="<class 'bool'>" and True!=False:
+    print(True,'is True!')
+if str(type(True))=="<class 'bool'>" and True!=False:
+    print(True,'is True!')
+if str(type(True))=="<class 'bool'>" and True!=False:
+    print(True,'is True!')
+if str(type(True))=="<class 'bool'>" and True!=False:
+    print(True,'is True!')
+if str(type(True))=="<class 'bool'>" and True!=False:
+    print(True,'is True!')
+if str(type(True))=="<class 'bool'>" and True!=False:
+    print(True,'is True!')
+if str(type(True))=="<class 'bool'>" and True!=False:
+    print(True,'is True!')
+if str(type(True))=="<class 'bool'>" and True!=False:
+    print(True,'is True!')
+if str(type(True))=="<class 'bool'>" and True!=False:
+    print(True,'is True!')
+if str(type(True))=="<class 'bool'>" and True!=False:
+    print(True,'is True!')
+if str(type(True))=="<class 'bool'>" and True!=False:
+    print(True,'is True!')
+if str(type(True))=="<class 'bool'>" and True!=False:
+    print(True,'is True!')
+if str(type(True))=="<class 'bool'>" and True!=False:
+    print(True,'is True!')
+if str(type(True))=="<class 'bool'>" and True!=False:
+    print(True,'is True!')
+if str(type(True))=="<class 'bool'>" and True!=False:
+    print(True,'is True!')
+if str(type(True))=="<class 'bool'>" and True!=False:
+    print(True,'is True!')
+if str(type(True))=="<class 'bool'>" and True!=False:
+    print(True,'is True!')
+if str(type(True))=="<class 'bool'>" and True!=False:
+    print(True,'is True!')
+if str(type(True))=="<class 'bool'>" and True!=False:
+    print(True,'is True!')
+if str(type(True))=="<class 'bool'>" and True!=False:
+    print(True,'is True!')
+if str(type(True))=="<class 'bool'>" and True!=False:
+    print(True,'is True!')
+if str(type(True))=="<class 'bool'>" and True!=False:
+    print(True,'is True!')
+if str(type(True))=="<class 'bool'>" and True!=False:
+    print(True,'is True!')
+if str(type(True))=="<class 'bool'>" and True!=False:
+    print(True,'is True!')
+if str(type(True))=="<class 'bool'>" and True!=False:
+    print(True,'is True!')
+if str(type(True))=="<class 'bool'>" and True!=False:
+    print(True,'is True!')
+if str(type(True))=="<class 'bool'>" and True!=False:
+    print(True,'is True!')
+if str(type(True))=="<class 'bool'>" and True!=False:
+    print(True,'is True!')
+if str(type(True))=="<class 'bool'>" and True!=False:
+    print(True,'is True!')
+if str(type(True))=="<class 'bool'>" and True!=False:
+    print(True,'is True!')
+if str(type(True))=="<class 'bool'>" and True!=False:
+    print(True,'is True!')
+if str(type(True))=="<class 'bool'>" and True!=False:
+    print(True,'is True!')
+if str(type(True))=="<class 'bool'>" and True!=False:
+    print(True,'is True!')
+if str(type(True))=="<class 'bool'>" and True!=False:
+    print(True,'is True!')
+if str(type(True))=="<class 'bool'>" and True!=False:
+    print(True,'is True!')
+if str(type(True))=="<class 'bool'>" and True!=False:
+    print(True,'is True!')
+if str(type(True))=="<class 'bool'>" and True!=False:
+    print(True,'is True!')
+if str(type(True))=="<class 'bool'>" and True!=False:
+    print(True,'is True!')
+if str(type(True))=="<class 'bool'>" and True!=False:
+    print(True,'is True!')
+if str(type(True))=="<class 'bool'>" and True!=False:
+    print(True,'is True!')
+if str(type(True))=="<class 'bool'>" and True!=False:
+    print(True,'is True!')
+if str(type(True))=="<class 'bool'>" and True!=False:
+    print(True,'is True!')
+if str(type(True))=="<class 'bool'>" and True!=False:
+    print(True,'is True!')
+if str(type(True))=="<class 'bool'>" and True!=False:
+    print(True,'is True!')
+if str(type(True))=="<class 'bool'>" and True!=False:
+    print(True,'is True!')
+if str(type(True))=="<class 'bool'>" and True!=False:
+    print(True,'is True!')
